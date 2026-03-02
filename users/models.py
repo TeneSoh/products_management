cec44ec4f8c892605be9ad754050a6bfc9fb5139
MiguelSouop <miguelsouop@gmail.com>
2026-03-02 18:42:31 +0100

    envoie du model user

--- users/models.py
+++ users/models.py
@@ -1,32 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-    class UserManager(BaseUserManager):
-        user_in_migration = True
-        def create_user(self, email, password=None, **extra_fields):
-            if not email:
-                raise ValueError("ce champ est requis")
-            email = self.normalize_email(email=email)
-            user = self.model(email=email, **extra_fields)
-            user.set_password(password)
-            user.save(using=self._db)
+class UserManager(BaseUserManager):
+    user_in_migration = True
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("ce champ est requis")
+        email = self.normalize_email(email=email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
-            return user
+        return user
 
-        def create_superuser(self, email, password=None, **extra_fields):
-            extra_fields.setdefault('is_staff', True)
-            extra_fields.setdefault('is_superuser', True)
-            extra_fields.setdefault('is_active', True)
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
 
-            return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, password, **extra_fields)
 
-    class User(AbstractUser):
-        email = models.EmailField(unique=True)
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
 
-        USERNAME_FIELD = "email"
-        REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
 
-        objects = UserManager()  # type: ignore
+    objects = UserManager()  # type: ignore
 
-        def __str__(self) -> str:
-            return self.email
+    def __str__(self) -> str:
+        return self.email
