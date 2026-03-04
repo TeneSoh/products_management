53b3e459a7c1bd43ef0b79fe1962e6d0a0c70d88
= <tenedouglas237@gmail.com>
2026-03-04 20:55:16 +0100

    suite

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+# from .views import ProductViewClass, ProductId
 from .views import ProductViewClass
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'product', ProductViewClass, basename='products')
 
 urlpatterns = [
-    path('', ProductViewClass.as_view())
+    # path('', ProductViewClass.as_view()),
+    path('', include(router.urls)),
+    # path('product/<int:pk>/', ProductId.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from rest_framework.response import Response
 
@@ -7,20 +8,33 @@ from .models import Product
 from .serializers import ProductSerializer
 from products.serializers import ProductSerializer
 
-class ProductViewClass(APIView):
-    def get(self, request):
-        products = Product.objects.all()
-        serialize = ProductSerializer(products, many=True)
+# class ProductViewClass(APIView):
+#     def get(self, request):
+#         products = Product.objects.all()
+#         serialize = ProductSerializer(products, many=True)
 
-        return Response(serialize.data)
+#         return Response(serialize.data)
     
-    def post(self, request):
-        product = ProductSerializer(data = request.data)
-        print(product)
-        if product.is_valid():
-            product.save()
+#     def post(self, request):
+#         product = ProductSerializer(data = request.data)
+#         print(product)
+#         if product.is_valid():
+#             product.save()
 
-            return Response(product.data, status=status.HTTP_201_CREATED)
+#             return Response(product.data, status=status.HTTP_201_CREATED)
         
-        return Response(product.errors, status=status.HTTP_400_BAD_REQUEST)
+#         return Response(product.errors, status=status.HTTP_400_BAD_REQUEST)
     
+
+# class ProductId(APIView):
+#     def get(self,request, pk):
+#         product = Product.objects.filter(pk = pk).first()
+#         # faite la verification
+#         serializer = ProductSerializer(product)
+
+#         return Response(serializer.data)
+    
+
+class ProductViewClass(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
